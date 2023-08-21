ab90572fc9de6ad4464d7925433d442701b04e7e
splatter0 <yangzhiwei.11@163.com>
2023-08-21 13:09:26 +0800

    fix

--- spring-batch-remote-partitioning-rabbitmq/src/main/java/com/splatter0/batch/manager/CustomerPartitioner.java
+++ spring-batch-remote-partitioning-rabbitmq/src/main/java/com/splatter0/batch/manager/CustomerPartitioner.java
@@ -3,15 +3,13 @@ package com.splatter0.batch.manager;
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.item.ExecutionContext;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CustomerPartitioner implements Partitioner {
 
     private static final String PARTITION_KEY = "partition";
 
+    @SuppressWarnings("NullableProblems")
     @Override
     public Map<String, ExecutionContext> partition(int gridSize) {
         var map = new HashMap<String, ExecutionContext>();
@@ -23,8 +21,9 @@ public class CustomerPartitioner implements Partitioner {
         for (int i = 0; i < 100; i++) {
             var key = PARTITION_KEY + (i % gridSize);
             var context = map.get(key);
+            //noinspection unchecked
             var ids = (List<Integer>) context.get("ids");
-            ids.add(i + 1);
+            Objects.requireNonNull(ids).add(i + 1);
         }
 
         return map;
--- spring-batch-remote-partitioning-rabbitmq/src/main/java/com/splatter0/batch/worker/WorkerConfiguration.java
+++ spring-batch-remote-partitioning-rabbitmq/src/main/java/com/splatter0/batch/worker/WorkerConfiguration.java
@@ -31,6 +31,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 import javax.sql.DataSource;
 
+@SuppressWarnings("DataFlowIssue")
 @Profile("worker")
 @Configuration
 @EnableBatchIntegration
