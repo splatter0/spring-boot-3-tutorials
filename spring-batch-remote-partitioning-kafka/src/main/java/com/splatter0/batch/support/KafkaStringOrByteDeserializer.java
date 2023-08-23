d0d2bec262039a8eb065bde9ed7ed0181d6450ce
splatter0 <yangzhiwei.11@163.com>
2023-08-23 09:38:45 +0800

    fix

--- spring-batch-remote-partitioning-kafka/src/main/java/com/splatter0/batch/support/KafkaStringOrByteDeserializer.java
+++ spring-batch-remote-partitioning-kafka/src/main/java/com/splatter0/batch/support/KafkaStringOrByteDeserializer.java
@@ -12,7 +12,6 @@ public class KafkaStringOrByteDeserializer implements Deserializer<Object> {
         try {
             return new DefaultDeserializer().deserialize(new ByteArrayInputStream(bytes));
         } catch (IOException e) {
-            e.printStackTrace();
             return null;
         }
     }
